123c51ac1c349615e72a98f1526e6b4feb305f3c
dienpv <chuanthanh133@gmail.com>
2025-04-08 14:38:18 +0700

    Initial commit

---
+++ test/aws-lex-learning.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsLexLearning from '../lib/aws-lex-learning-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-lex-learning-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsLexLearning.AwsLexLearningStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
